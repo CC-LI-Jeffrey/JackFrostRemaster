c27ac95ff6c61178eea72435372420ce5a5be738
Chunggg <jeffreyli12386@gmail.com>
2024-07-24 17:35:56 +0800

    character control, map collider

---
+++ Assets/Scripts/CharacterScript.cs
@@ -0,0 +1,52 @@
+using System;
+using UnityEngine;
+
+public class CharacterScript : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    [SerializeField] private float jumpingPower;
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private LayerMask groundLayer;
+    private bool isFacingLeft;
+    private float horizontal;
+    private Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontal = Input.GetAxis("Horizontal");
+
+        if (Input.GetKeyDown(KeyCode.W) && IsGrounded())
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpingPower);
+        }
+
+        Flip();
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
+    }
+
+    private bool IsGrounded()
+    {
+        return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
+    }
+
+    private void Flip()
+    {
+        if (isFacingLeft && horizontal > 0f || !isFacingLeft && horizontal < 0f)
+        {
+            isFacingLeft = !isFacingLeft;
+            Vector3 localScale = transform.localScale;
+            localScale.x *= -1f;
+            transform.localScale = localScale;
+        }
+    }
+}
